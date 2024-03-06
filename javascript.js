0395b3426008b7c52cd82d70c5d86e39ef6e52d2
Giovanni Useche <giovanni.useche@hotmail.com>
2024-03-06 11:52:56 -0500

    end

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-function toyCar(brand, model, color, year, price, feature, made, warning, recycle, barcode, tires, size, baseColor, roofColor) {
+const output = document.getElementById('toyCarContainer');
+
+function toyCar(brand, model, color, year, price, feature, made, seats, recycle, barcode, tires, size, baseColor, roofColor) {
     this.brand = brand;
     this.model = model;
     this.color = color;
@@ -6,7 +8,7 @@ function toyCar(brand, model, color, year, price, feature, made, warning, recycl
     this.price = price;
     this.feature = feature;
     this.made = made;
-    this.warning = warning;
+    this.seats = seats;
     this.recycle = recycle;
     this.barcode = barcode;
     this.tires = tires;
@@ -15,11 +17,12 @@ function toyCar(brand, model, color, year, price, feature, made, warning, recycl
     this.roofColor = roofColor;
 
     this.describe = function() {
-        let description = `The brand is ${this.brand} and the model is ${this.model} the color is ${this.color}, year ${this.year} `;
+        let description = `The brand is ${this.brand} and the model is ${this.model} the color is ${this.color}, develop year ${this.year}, the price is ${this.price}, the feature ${this.feature}, develop in ${this.made}, number of seats ${this.seats}, is ${this.recycle}, barcode ${this.barcode}, number of tires ${this.tires}, car size ${this.size}, base color ${this.baseColor}, and roof color ${this.roofColor} `;
+
         return description;
     }
 }
 
-const toyCar = new toyCar(brand = 'Mega', Model = '1186 MJ.1.NL')
+const toyCar1 = new toyCar('Mega', '1186 MJ.1.NL', 'Red, gray and Blue', 2022, '7.00 Cad', 'high speed', 'Malaysia', 2, 'Yes', 2708412013, 4, 'small', 'Red', 'Blue');
 
-output.textContent = toyCar.describe();
+output.textContent = toyCar1.describe();
